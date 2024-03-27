87370f1d5d0a9459ae0afb54b319752d5a1701f7
Anaks-CT <anaksthayyil30@gmail.com>
2024-03-27 19:49:00 +0530

    adds boilerplate for auth-routes

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,7 +1,16 @@
 import express from "express";
+import { currentuserRouter } from "./routes/current-user";
+import { signinRouter } from "./routes/signin";
+import { signoutRouter } from "./routes/signout";
+import { signupRouter } from "./routes/signup";
 
 const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
-app.listen(3000, () => console.log('listening on port 3000'));
+app.use(currentuserRouter);
+app.use(signinRouter);
+app.use(signoutRouter);
+app.use(signupRouter);
+
+app.listen(3000, () => console.log("listening on port 3000"));
