3ea0db2cf2fe59a421ae03fc37b72f4a3ee6452a
Anaks-CT <anaksthayyil30@gmail.com>
2024-03-27 23:06:18 +0530

    adds express-async-error handler

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,8 +1,11 @@
 import express from "express";
+import 'express-async-errors';
 import { currentuserRouter } from "./routes/current-user";
 import { signinRouter } from "./routes/signin";
 import { signoutRouter } from "./routes/signout";
 import { signupRouter } from "./routes/signup";
+import { errorHandler } from "./middlewares/error-handler";
+import { NotFoundError } from "./errors/not-found-error";
 
 const app = express();
 
@@ -12,5 +15,11 @@ app.use(currentuserRouter);
 app.use(signinRouter);
 app.use(signoutRouter);
 app.use(signupRouter);
+
+app.all("*", () => {
+  throw new NotFoundError();
+});
+
+app.use(errorHandler);
 
 app.listen(3000, () => console.log("listening on port 3000"));
--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,24 +1,26 @@
-import express from "express";
+import express, { Request, Response } from "express";
 import { signupValidation } from "../validations";
-import asyncHandler from "express-async-handler";
 import { validationResult } from "express-validator";
+import { RequestValidationError } from "../errors/request-validation-error";
+import { DatabaseConnectionError } from "../errors/database-connection-error";
+import "express-async-errors";
 
 const router = express.Router();
 
 router.post(
   "/api/users/signup",
   signupValidation,
-  asyncHandler((req, res) => {
+  (req: Request, res: Response) => {
     const errors = validationResult(req);
 
     if (!errors.isEmpty()) {
-       res.status(400).send(errors.array());
+      throw new RequestValidationError(errors.array());
     }
+    console.log("Creating a user...");
+    throw new DatabaseConnectionError();
 
-    console.log('Creating a user...')
-
-    res.send({})
-  })
+    res.send({});
+  }
 );
 
 export { router as signupRouter };
