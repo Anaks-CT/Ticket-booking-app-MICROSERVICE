15018d365ea79ed3a95cd25dc1062935e75c63e5
Anaks-CT <anaksthayyil30@gmail.com>
2024-03-29 19:14:04 +0530

    finishes authentication adds cookie-session to store jwt adds jsonwebtoken to authenticate adds secret object to kubernetes for env variables

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,16 +1,29 @@
 import express from "express";
-import 'express-async-errors';
+import "express-async-errors";
+import cookieSession from "cookie-session";
+
 import { currentuserRouter } from "./routes/current-user";
 import { signinRouter } from "./routes/signin";
 import { signoutRouter } from "./routes/signout";
 import { signupRouter } from "./routes/signup";
+
 import { errorHandler } from "./middlewares/error-handler";
 import { NotFoundError } from "./errors/not-found-error";
 import mongoose from "mongoose";
 
 const app = express();
-
+app.set("trust proxy", true); // trust the proxy that is coming from ingress enginx traffics
 app.use(express.json());
+
+app.use(
+  cookieSession({
+    // we dont want to encrypt the cockie because if we are using other backend architecture
+    // using ruby rails its difficult with encryped cookies.
+    // on top of that we are using tamper resistant JWT. so no worries.
+    signed: false,
+    secure: true, // only on https
+  })
+);
 
 app.use(currentuserRouter);
 app.use(signinRouter);
@@ -21,17 +34,19 @@ app.all("*", () => {
   throw new NotFoundError();
 });
 
-
 app.use(errorHandler);
 
 const start = async () => {
+  if (!process.env.JWT_SECRET_KEY) {
+    throw new Error("JWT_SECRET_KEY must be defined");
+  }
   try {
-    await mongoose.connect('mongodb://auth-mongo-srv:27017/auth')
-    console.log("connected to mongoDB")
+    await mongoose.connect("mongodb://auth-mongo-srv:27017/auth");
+    console.log("connected to mongoDB");
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
   app.listen(3000, () => console.log("listening on port 3000"));
-}
+};
 
-start()
+start();
--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -3,6 +3,7 @@ import { signupValidation } from "../validations";
 import { validationResult } from "express-validator";
 import { RequestValidationError } from "../errors/request-validation-error";
 import "express-async-errors";
+import jwt from "jsonwebtoken";
 import { User } from "../models/user";
 import { BadRequestError } from "../errors/bad-request-error";
 
@@ -19,10 +20,25 @@ router.post(
     const { email, password } = req.body;
     const existingUser = await User.findOne({ email });
 
-    if (existingUser) throw new BadRequestError('User already exists');
+    if (existingUser) throw new BadRequestError("User already exists");
 
-    const user = User.build({ email, password})
+    const user = User.build({ email, password });
     await user.save();
+
+    // Generate token
+    const userjwt = jwt.sign(
+      {
+        id: user.id,
+        email: user.email,
+      },
+      process.env.JWT_SECRET_KEY!,
+      { expiresIn: 3600 }
+    );
+
+    // Store it on session
+    req.session = {
+      jwt: userjwt,
+    };
 
     res.status(201).send(user);
   }
